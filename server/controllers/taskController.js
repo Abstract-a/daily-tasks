49667d34b4c5919124667840b4b9740455d08eac
Abstract-a <m.kamari100@gmail.com>
2024-09-20 17:52:42 +0100

    wip

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,21 +1,46 @@
-const getTasks = (req, res) => {
-  res.status(200).json({ message: 'Get all tasks' });
-};
+const asyncHandler = require('express-async-handler');
+const Task = require('../models/taskModel.js');
 
-const setTask = (req, res) => {
+// GET
+const getTasks = asyncHandler(async (req, res) => {
+  const tasks = await Task.find();
+  res.status(200).json(tasks);
+});
+// POST
+const setTask = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Plese enter a task');
   }
-  res.status(200).json({ message: 'Create task' });
-};
+  const task = await Task.create({ text: req.body.text });
+  res.status(200).json(task);
+});
 
-const updateTask = (req, res) => {
-  res.status(200).json({ message: `Task ${req.params.id} updated` });
-};
+// PUT
+const updateTask = asyncHandler(async (req, res) => {
+  const task = await Task.findById(req.params.id);
 
-const deleteTask = (req, res) => {
-  res.status(200).json({ message: `Task ${req.params.id} deleted` });
-};
+  if (!task) {
+    res.status(400);
+    throw new Error('Task not found');
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedTask);
+});
+
+//DELETE
+const deleteTask = asyncHandler(async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  if (!task) {
+    res.status(400);
+    throw new Error('Task not found');
+  }
+  await Task.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = { getTasks, setTask, updateTask, deleteTask };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./connect/database.js');
 const dotenv = require('dotenv').config();
 
-const app = express();
+connectDB();
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
