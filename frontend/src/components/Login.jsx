b92d9e0f76f5ca5d72b36e874997b7434eba010b
Abstract-a <m.kamari100@gmail.com>
2024-10-14 00:05:52 +0100

    wip

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start creating a task</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
